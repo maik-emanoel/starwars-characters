6d993865a3b31ebdcfc9862ccdb7d94534977c80
Maik Emanoel <maikemanoel11@gmail.com>
2023-05-08 09:17:40 -0300

    update

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-let leftCard = document.querySelector('.left');
-let middleCard = document.querySelector('.middle');
-let rightCard = document.querySelector('.right');
+let leftCard = document.querySelector('.left')
+let middleCard = document.querySelector('.middle')
+let rightCard = document.querySelector('.right')
 
 leftCard.addEventListener('click', () => {
-	let temp = middleCard.innerHTML;
-	middleCard.innerHTML = leftCard.innerHTML;
-	leftCard.innerHTML = temp;
+	let temp = middleCard.innerHTML
+	middleCard.innerHTML = leftCard.innerHTML
+	leftCard.innerHTML = temp
 });
 
 rightCard.addEventListener('click', () => {
-	let temp = middleCard.innerHTML;
-	middleCard.innerHTML = rightCard.innerHTML;
-	rightCard.innerHTML = temp;
-});
+	let temp = middleCard.innerHTML
+	middleCard.innerHTML = rightCard.innerHTML
+	rightCard.innerHTML = temp
+})
